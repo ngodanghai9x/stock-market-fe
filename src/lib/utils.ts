f3bcdc97dad30fbdf0783b608f5181188a97b176
Hai <ngodanghai9x@gmail.com>
2022-01-31 12:17:01 +0700

    add types, constants, utils function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,18 @@ import { JWT_TOKEN } from '../constants/request';
 import { LoginPayload } from '../services/api-auth.type';
 import { TokenInfo } from '../types';
 import { customerLogin } from './../services/api-auth.service';
+// import crypto from 'crypto';
+
+// export const generateOTP = async (bytes: number = 256): Promise<string> => {
+//   return new Promise((resolve, reject) => {
+//     crypto.randomBytes(bytes, (err, buf) => {
+//       if (err) reject(err);
+
+//       const token = crypto.createHash('sha1').update(buf).digest('hex');
+//       resolve(token);
+//     });
+//   });
+// };
 
 export const login = async (data: LoginPayload) => {
   try {
@@ -24,4 +36,8 @@ export const getTokenData = (token: string): TokenInfo | null => {
   } catch (error) {
     return null;
   }
+};
+
+export const isEmpty = (value: string | number): boolean => {
+  return value === null || value === undefined || String(value).trim() === '';
 };
--- src/types.d.ts
+++ src/types.d.ts
@@ -8,3 +8,42 @@ export interface TokenInfo {
 }
 
 export type Value = string | number | boolean | null;
+
+export type LoginReq = {
+  username: string;
+  password: string;
+};
+
+export type LogoutReq = {};
+
+export type RegisterReq = {
+  username: string;
+  password: string;
+  fullName: string;
+  email: string;
+};
+
+export type ForgetPwReq = {
+  username: string;
+  password: string;
+  otpForget: string;
+};
+
+export type ChangPwReq = {
+  password: string;
+  oldPassword: string;
+};
+
+export type CreateCompanyReq = {
+  company: Company;
+  isIpo: boolean;
+  account: { password: string; username: string };
+  // stock: UserStorage;
+};
+
+export type ValidCreateCompanyReq = {
+  company: Company;
+  isIpo: boolean;
+  user: User;
+  // stock: UserStorage;
+};

40d39c655d6807f1943bb7a026811f558b327119
anhtuan <iamhaphong15091999@gmail.com>
2022-04-10 22:23:41 +0700

    Temp breadcrumbs

--- src/pages/admin/company/index.tsx
+++ src/pages/admin/company/index.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 
 const CompanyPage = () => {
   return (
-    // <Button variant="text">Text</Button>
     <div className='bg-white h-full w-3/4'>
-      <Button variant='contained' >alo</Button>
     </div>
   )
 }
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,15 +1,36 @@
-import { Outlet } from "react-router-dom"
+import { Breadcrumbs, Link, Typography } from "@mui/material"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../../components/admin/layout/header"
 import SideBar from "../../components/admin/layout/side-bar"
 
 const AdminPage = () => {
+
+  // const { pathname } = useLocation();
+
   return (
     <div className="grid grid-cols-12 w-full">
       <SideBar />
       <div className='bg-slate-50 col-span-10'>
         <Header />
         <div className="bg-gray-100 h-[calc(100vh-64px)]">
-          <div className=" flex h-full items-start justify-center py-10">
+          <div>
+          </div>
+          <div className=" flex h-full items-center py-10 flex-col">
+            <div className="w-3/4 pb-2">
+              {/* <Breadcrumbs aria-label="breadcrumb">
+                <Link underline="hover" color="inherit" href="/">
+                  MUI
+                </Link>
+                <Link
+                  underline="hover"
+                  color="inherit"
+                  href="/material-ui/getting-started/installation/"
+                >
+                  Core
+                </Link>
+                <Typography color="text.primary">Breadcrumbs</Typography>
+              </Breadcrumbs> */}
+            </div>
             <Outlet />
           </div>
         </div>
